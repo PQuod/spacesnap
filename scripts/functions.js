33645c7f97764ef83c033029881957ea759ed465
unknown <l.desaubliaux@gmail.com>
2023-05-25 12:35:11 +0200

    Adding V2.0

---
+++ scripts/functions.js
@@ -0,0 +1,291 @@
+// array of the current y axis
+let currentImgsArray = [];
+
+// Variables of positions
+let horizonPosition = 3;
+let nivHyper = 8;
+let nivAtmos = 6;
+let nivAbys = 0;
+
+//X & Y first positions
+let posX = 0;
+let posY = horizonPosition;
+
+let starter = true;
+
+//Event listener on key down
+document.onkeydown = checkKey;
+document.onkeyup = keyboardWhite;
+checkPosY();
+
+//Check where on Y axis we are and which array to assign to currentImgsArray
+function checkPosY() {
+    currentImgsArray = imgsLatitudes[posY];
+    // Repositioning to last posX on new Y axis in case former Y axis has fewer pics and posX is too far away
+    if (posX > currentImgsArray.length) {
+        posX = currentImgsArray.length;
+    }
+}
+
+
+//Function on key down
+function checkKey(event) {
+    console.log(event);
+    //Start (Hit 'Enter', display tuto)
+    if(starter == true && (event.keyCode == '13' || event == '13')){
+        document.canvas.src = "blank.jpg";
+        playSound('sounds/launching.wav');
+        document.getElementById("tuto").style.display = "inline-block";
+        document.getElementById("demarrage").style.display = "none";
+        document.getElementById("fintuto").style.display = "inline-block";
+        starter = false;
+        displayKeyboard();       
+    }
+    //Hit Enter again (display img posX = 1 and posY = horizon, hide start messages, display keyboard and menu)
+    else if (posX == 0 && posY == horizonPosition) {
+        if (event.keyCode == '13' || event == '13') {
+            document.getElementById("tuto").style.display = "none";
+            document.getElementById("fintuto").style.display = "none";
+            document.getElementById("welcome").style.display = "none";
+            posX += 1;
+            loadImg();
+            displayKeyboard();
+            displayElement(document.getElementById("menu"));
+            generateMap(document.getElementById("map"), document.
+            getElementById("anchormap"));
+            generateFeat();
+            locateOnMap(document.getElementById("map"));
+        }
+    } else {
+        // Hit 'Echap' to display credits
+        if (event.keyCode == '27' || event == '27'){
+            displayFeat();
+        }
+        if(document.getElementById("credits-container").style.display !== "inline-block"){
+            // Hit 'i' to toggle legend
+            if (event.keyCode == '73' || event == '73') {
+                displayElement(document.getElementById("legende"));
+            }
+            // Hit 'm' to toggle map
+            if (event.keyCode == '77' || event == '77'){
+                displayElement(document.getElementById('map'));
+            }
+
+            //DIRECTIONS
+            // If right arrow is pressed and we haven't reached last position on the X axis
+            if ((event.keyCode == '39' || event == '39') && (posX < currentImgsArray.length) && (posX != 0 || posY != 0)) {
+                if (posX < currentImgsArray.length) {
+                    posX += 1;
+                }
+            }
+            // If left arrow is pressed and we haven't reach first position on the X axis
+            else if ((event.keyCode == '37' || event == '37') && (posX > 1)) {
+                posX -= 1;
+            }
+            // If up arrow is pressed and we have not reached top of Y axis
+            else if ((event.keyCode == '38' || event == '38') && (posY < imgsLatitudes.length - 1)) {
+                posY += 1;
+                checkPosY();
+            }
+            // If down arrow is pressed and we have not reached bottom of Y axis
+            else if ((event.keyCode == '40' || event == '40') && (posY > 0)) {
+                posY -= 1;
+                checkPosY();
+            }
+        }
+        playSound('sounds/deplacement.wav');
+        //Function calls at each keydown
+        locateOnMap(document.getElementById("map"));
+        allSystemsDisconnected(document.getElementById("errormessage"));
+        loadImg();
+        keyboardRed(event);
+        legendGenerate(document.getElementById("legende"));
+    }
+}
+
+
+// Change canvas src with the corresponding X & Y img
+function loadImg() {
+    if (document.getElementById("credits-container").style.display == "none"){
+        for (let i = 0; i < currentImgsArray.length; i++) {
+            if (currentImgsArray.indexOf(currentImgsArray[i]) + 1 == posX) {
+                document.canvas.src = currentImgsArray[i][0];
+            }
+        }
+    }else{
+        document.canvas.src = "imgs/sayonara.jpg";
+    }
+}
+
+function displayElement(element){
+    if(element.style.display == "none"){
+        element.style.display = "inline-block";
+    }else{
+        element.style.display = "none";
+    }
+}
+
+function displayFeat(){
+    let creditsContainer = document.getElementById("credits-container");
+    if(creditsContainer.style.display == "none"){
+        document.getElementById("map").style.display = "none";
+        document.getElementById("arrowkeyboard").style.display = "none";
+        document.getElementById("legende").style.display = "none";
+        creditsContainer.style.display = "inline-block";
+        creditsContainer.style.height = "100vh";
+    }else{
+        creditsContainer.style.display = "none";
+        document.getElementById("arrowkeyboard").style.display = "grid";
+    }
+}
+
+// Coloration des touches activées
+let activeKey = "";
+
+function displayKeyboard() {
+    let tuto = document.getElementById("tuto");
+    if ((posX != 0) && tuto.style.display == "none") {
+        document.getElementById("enterkey").style.display = "none";
+        let arrowkeys = document.getElementsByClassName("arrowkey");
+        for(let i = 0; i < arrowkeys.length; i++){
+            arrowkeys[i].style.display = "inline-block";
+        }
+    }
+}
+
+// Cartographie
+function generateMap(parentDiv, anchorMap) {
+    for (let i = imgsLatitudes.length - 1; i >= 0; i--) {
+        let spanNiv = document.createElement("span");
+        //Generating one box for each img
+        for (let j = 0; j < imgsLatitudes[i].length; j++) {
+            const newP = document.createElement("p");
+            newP.classList.add('inline-p');
+            newP.classList.add(i + '_' + j);
+            parentDiv.insertBefore(newP, anchorMap);
+        }
+        //Generating spans for each big "step", according to variables declared previously
+        if(i== nivHyper){         
+            spanNiv.innerHTML = "<br>Hyperespace<br>";         
+        }
+        if(i == nivAtmos){
+            spanNiv.innerHTML = "<br>Atmosphère<br>";
+        }
+        if (i == horizonPosition) {
+            spanNiv.innerHTML = "<br>Niveau de la mer<br>";
+        }
+        if(i == nivAbys){
+            spanNiv.innerHTML = "<br>Abysses<br>";
+        }
+        parentDiv.insertBefore(spanNiv, anchorMap);
+        //Inserting <br> at the end of each Y positions
+        const newBr = document.createElement("br");
+        parentDiv.insertBefore(newBr, anchorMap);
+    }
+}
+
+function locateOnMap(map) {
+    let imgPosX = 0;
+    let imgPosY = 0;
+    let children = map.getElementsByTagName('p');
+    for (let i = 0; i < children.length; i++) {
+        imgPositions = (children[i].className.slice(children[i].className.indexOf(' ') + 1));
+        imgPositions = imgPositions.split('_');
+        imgPosX = parseInt(imgPositions[1]) + 1;
+        imgPosY = parseInt(imgPositions[0]);
+        if (posX == imgPosX && posY == imgPosY) {
+            children[i].style.borderColor = 'red';
+            children[i].style.background = "red";
+        } else {
+            children[i].style.borderColor = 'white';
+            children[i].style.background = "none";
+        }
+    }
+}
+
+// Affichage des légendes
+function legendGenerate(legende) {
+    for (let i = 0; i < currentImgsArray.length; i++) {
+        if (i + 1 == posX) {
+            legende.innerHTML = currentImgsArray[i][1] + ' &middot; ' + currentImgsArray[i][2];
+        }
+    }
+}
+
+function keyboardRed(event) {
+    if (event.keyCode == '39') {
+        activeKey = "arrowright";
+    } else if (event.keyCode == '37') {
+        activeKey = "arrowleft";
+    } else if (event.keyCode == '38') {
+        activeKey = "arrowup";
+    } else if (event.keyCode == '40') {
+        activeKey = "arrowdown";
+    } else if (event.keyCode == '77') {
+        activeKey = "mapkey";
+    } else if (event.keyCode == '27') {
+        activeKey = "creditskey";
+    } else if (event.keyCode == '73') {
+        activeKey = "legendkey";
+    } else if (event.keyCode == '13') {
+        activeKey = "enterkey";
+    }
+    if (activeKey !== "") {
+        document.getElementById(activeKey).style.border = "1px solid red";
+        document.getElementById(activeKey).style.color = "red";
+    }
+}
+
+function keyboardWhite() {
+    if (activeKey !== "") {
+        document.getElementById(activeKey).style.border = "1px solid white";
+        document.getElementById(activeKey).style.color = "white";
+        activeKey = "";
+    }
+}
+
+function allSystemsDisconnected(error) {
+    let map = document.getElementById("map");
+    let menu = document.getElementById("menu");
+    if (posY == imgsLatitudes.length - 1) {
+        playSound('sounds/error.wav');
+        map.style.opacity = "0";
+        menu.style.opacity = "0";
+        error.style.display = "inline-block";
+        error.innerHTML = "↓↓↓↓&²@k↓<br>Altitude maximale atteinte //<br>surch4uffe f&jk% des systèmesV3uillez reDesc3ndre avant la fus1on des roéaiohcteurs<br>"
+    } else {
+        error.style.display = "none";
+        menu.style.opacity = "1";
+        map.style.opacity = "1";
+    }
+}
+
+function generateFeat(){
+    const creditCont = document.getElementById("credits-container");
+    const anchorFeat = document.getElementById("anchorfeat");
+    let featList = [];
+    
+    for(i = 0; i < imgsLatitudes.length; i++){
+        for(j = 0; j < imgsLatitudes[i].length; j++){
+            featList.push(imgsLatitudes[i][j][1]);  
+        }
+    }
+    featList.push(imgCredit);
+    featList = featList.sort();
+    featList.push(soundCredit);
+    console.log(featList);
+    for(i = 0; i < featList.length; i++){
+        if(featList[i] != featList[i-1])
+        {const newP = document.createElement("p");
+        newP.innerHTML = featList[i];
+        creditCont.insertBefore(newP, anchorFeat);}
+    }
+}
+
+function playSound(sound){
+    var audio = new Audio(sound);
+        audio.volume = 0.4;
+    audio.play();
+}
+
+
---
+++ scripts/imgsarrays.js
@@ -0,0 +1,136 @@
+let imgCredit = "<i>Sayonara Wild Hearts</i>, Simogo, Annapurna Interactive, 2019"
+let soundCredit = "<br><p>Sons : <i>Pinball 3D Space Cadet</i>, Microsoft, 1995</p>"
+// Arrays depending on Y axis
+let imgsLatitudes = [
+    //Abysses
+    [
+        ["imgs/quern2.jpg", "<i>Quern, Undying thoughts</i>, Zadbox Entertainment, 2016", "Prise de vue : 18/03/2022"],
+        ["imgs/quern3.jpg", "<i>Quern, Undying thoughts</i>, Zadbox Entertainment, 2016", "Prise de vue : 18/03/2022"],
+        ["imgs/quern1.jpg", "<i>Quern, Undying thoughts</i>, Zadbox Entertainment, 2016", "Prise de vue : 18/03/2022"],
+        ["imgs/abzu3.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],
+        ["imgs/abzu7.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],   
+        ["imgs/abzu4.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],
+    ],
+
+    [
+        ["imgs/abzu6.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],
+        ["imgs/abzu2.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],
+        ["imgs/oldman1.jpg", "<i>Old Man's Journey</i>, Broken Rules, 2017", "Prise de vue : 14/10/2020."],
+        ["imgs/callofthesea2.jpg", "<i>Call of the Sea</i>, Out of the Blue Games, Raw of Fury, 2020", "Prise de vue : 02/02/2022"],
+        ["imgs/abzu5.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"],
+        ["imgs/davethediver1.jpg", "<i>Dave the Diver</i>, Mintrocket, 2022 (earlyaccess)", "Prise de vue : 01/04/2023"],
+    ],
+
+    [
+        ["imgs/beforeyoureyes1.jpg", "<i>Before your eyes</i>, GoodbyeWorld Games, Skybound Games, 2021", "Prise de vue : 26/08/2022"],
+        ["imgs/thelastcampfire01.jpg", "<i>The Last Campfire</i>, Hello Games, 2021", "Prise de vue : 19/10/2022"]
+    ],
+    //Niveau de la mer
+    [
+        ["imgs/beginner4.jpg", "<i>The Beginner's Guide</i>, Everything Unlimited LTD, 2015", "Prise de vue : 28/01/2017."],
+        ["imgs/beginner3.jpg", "<i>The Beginner's Guide</i>, Everything Unlimited LTD, 2015", "Prise de vue : 28/01/2017."],
+        ["imgs/beginner5.jpg", "<i>The Beginner's Guide</i>, Everything Unlimited LTD, 2015", "Prise de vue : 28/01/2017."],
+        ["imgs/stillness2.jpg", "<i>The Stillness of the wind</i>, Memory of gold / Lambic Studio, Fellow Traveller, 2019", "Prise de vue : 12/03/2021."],
+        ["imgs/stillness1.jpg", "<i>The Stillness of the wind</i>, Memory of gold / Lambic Studio, Fellow Traveller, 2019", "Prise de vue : 12/03/2021."],
+        ["imgs/far3.jpg", "<i>FAR: Lone Sail</i>, Okomotive, Mixtvision, 2018", "Prise de vue : 26/10/2020."],
+        ["imgs/far1.jpg", "<i>FAR: Lone Sail</i>, Okomotive, Mixtvision, 2018", "Prise de vue : 26/10/2020."],
+        ["imgs/phoning1.jpg", "<i>Phoning Home</i>, Ion Lands, 2017", "Prise de vue : 09/01/2018."],
+        ["imgs/blackwood.jpg", "<i>Blackwood Crossing</i>, Paper Seven LTD, Vision Games Publishing LTD, 2017", "Prise de vue : 03/10/2020."],
+        ["imgs/crossingsouls.jpg", "<i>Crossing Souls</i>, Fourattic, Devolver Digital, 2018", "Prise de vue : 24/05/2020."],
+        ["imgs/norwood2.jpg", "<i>The Norwood Suite</i>, Cosmo D, Alliance, 2017", "Prise de vue : 10/03/2019"],
+        ["imgs/oldman2.jpg", "<i>Old Man's Journey</i>, Broken Rules, 2017", "Prise de vue : 14/10/2020."],
+        ["imgs/wewerehereforever3.jpg", "<i>We were here forever</i>, Total Mayhem Games, 2022", "Prise de vue : 29/01/2023"],
+        ["imgs/abzu1.jpg", "<i>Abzu</i>, Giant Squid, 505 Games, 2016", "Prise de vue: 01/04/2023"]
+    ],
+
+    [
+        ["imgs/beforeyoureyes2.jpg", "<i>Before your eyes</i>, GoodbyeWorld Games, Skybound Games, 2021", "Prise de vue : 26/08/2022"],
+        ["imgs/beforeyoureyes3.jpg", "<i>Before your eyes</i>, GoodbyeWorld Games, Skybound Games, 2021", "Prise de vue : 26/08/2022"],
+        ["imgs/room1.jpg", "<i>The Room III</i>, Fireproof Games, 2018", "Prise de vue : 22/12/2019."],
+        ["imgs/callofthesea1.jpg", "<i>Call of the Sea</i>, Out of the Blue Games, Raw of Fury, 2020", "Prise de vue : 02/02/2022"],
+        ["imgs/magiccircle.jpg", "<i>The Magic Circle</i>, Question, 2015", "Prise de vue : 15/12/2018."],
+        ["imgs/monsterhunterrise.jpg", "<i>Monster Hunter Rise</i>, Capcom, 2022", "Prise de vue : 10/09/2022"],
+        ["imgs/far2.jpg", "<i>FAR: Lone Sail</i>, Okomotive, Mixtvision, 2018", "Prise de vue : 26/10/2020."],
+        ["imgs/phoning2.jpg", "<i>Phoning Home</i>, Ion Lands, 2017", "Prise de vue : 09/01/2018."],
+        ["imgs/rime1.jpg", "<i>RiME</i>, Tequila Works, QLOC, Grey Box, Six Foot, 2017", "Prise de vue : 13/11/2018."],
+        ["imgs/rime2.jpg", "<i>RiME</i>, Tequila Works, QLOC, Grey Box, Six Foot, 2017", "Prise de vue : 24/11/2018."],
+        ["imgs/norwood1.jpg", "<i>The Norwood Suite</i>, Cosmo D, Alliance, 2017", "Prise de vue : 10/03/2019."],
+        ["imgs/zeldabotw.jpg", "<i>The Legend of Zelda: Breath of the Wild</i>, Nintendo, Monolith Soft, 2017", "Prise de vue: 06/08/2022."],
+        ["imgs/quern4.jpg", "<i>Quern, Undying thoughts</i>, Zadbox Entertainment, 2016", "Prise de vue : 18/03/2022"],
+        ["imgs/quern5.jpg", "<i>Quern, Undying thoughts</i>, Zadbox Entertainment, 2016", "Prise de vue : 18/03/2022"],
+        ["imgs/room2.jpg", "<i>The Room III</i>, Fireproof Games, 2018", "Prise de vue : 22/12/2019."],
+        ["imgs/reddead.jpg", "<i>Red Dead Redemption II</i>, Rockstar Games, 2019", "Prise de vue : 17/04/2020."],
+        ["imgs/rapture1.jpg", "<i>Everybody's gone to the rapture</i>, The Chinese Room, LLC, 2016", "Prise de vue : 07/01/2017."],
+        ["imgs/rapture2.jpg", "<i>Everybody's gone to the rapture</i>, The Chinese Room, LLC, 2016", "Prise de vue : 08/01/2017."],
+        ["imgs/rapture3.jpg", "<i>Everybody's gone to the rapture</i>, The Chinese Room, LLC, 2016", "Prise de vue : 08/01/2017."],
+        ["imgs/beginner1.jpg", "<i>The Beginner's Guide</i>, Everything Unlimited LTD, 2015", "Prise de vue : 28/01/2017."],
+        ["imgs/wewerehereforever2.jpg", "<i>We were here forever</i>, Total Mayhem Games, 2022", "Prise de vue : 29/01/2023"],
+        ["imgs/wewerehereforever1.jpg", "<i>We were here forever</i>, Total Mayhem Games, 2022", "Prise de vue : 29/01/2023"],
+    ],
+
+    [
+        ["imgs/spiritfarer1.jpg", "<i>Spiritfarer</i>, Thunder Lotus Games, 2020", "Prise de vue: 28/12/2021."],
+        ["imgs/thelastcampfire02.jpg", "<i>The Last Campfire</i>, Hello Games, 2021", "Prise de vue : 19/10/2022"],
+        ["imgs/baldur.jpg", "<i>Baldur's Gate III</i>, Larian Studios, 2020", "Prise de vue : 01/09/2021."],
+        ["imgs/fidelio1.jpg", "<i>The Fidelio Incident</i>, Act 3 Games, LLC, 2017", "Prise de vue : 25/03/2018."],
+        ["imgs/fidelio2.jpg", "<i>The Fidelio Incident</i>, Act 3 Games, LLC, 2017", "Prise de vue : 25/03/2018."],
+        ["imgs/alittletotheleft.jpg", "<i>A little to the left</i>, Max Inferno, Secret Mode, 2022", "Prise de vue : 12/01/2023."],
+        ["imgs/spiritfarer3.jpg", "<i>Spiritfarer</i>, Thunder Lotus Games, 2020", "Prise de vue: 28/12/2021."],
+        ["imgs/spiritfarer2.jpg", "<i>Spiritfarer</i>, Thunder Lotus Games, 2020", "Prise de vue: 28/12/2021."],
+        ["imgs/spiritfarer4.jpg", "<i>Spiritfarer</i>, Thunder Lotus Games, 2020", "Prise de vue: 28/12/2021."]
+    ],
+    
+//Atmosphère
+    [
+        ["imgs/beginner2.jpg", "<i>The Beginner's Guide</i>, Everything Unlimited LTD, 2015", "Prise de vue : 28/01/2017"],
+        ["imgs/observation7.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/observation8.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/observation9.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/starbound.jpg", "<i>Starbound</i>, Chucklefish, 2016", "Prise de vue : 17/11/2019."],
+        ["imgs/pokemonecarlate3.jpg", "<i>Pokémon Ecarlate</i>, Game Freak, Nintendo, 2022", "Prise de vue: 10/11/2022."],
+
+    ],
+
+    [
+        ["imgs/vanishing2.jpg", "<i>The Vanishing of Ethan Carter</i>, The Astronauts, 2014", "Prise de vue : 30/12/2016."],
+        ["imgs/observation4.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/observation6.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/spaceplan1.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/detective2.jpg", "<i>The Darkside Detective</i>, Spooky Doorway, Akapura Games, 2017", "Prise de vue : 02/11/2019."],
+        ["imgs/spaceplan2.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/vanishing1.jpg", "<i>The Vanishing of Ethan Carter</i>, The Astronauts, 2014", "Prise de vue : 30/12/2016."],
+        ["imgs/vanishing3.jpg", "<i>The Vanishing of Ethan Carter</i>, The Astronauts, 2014", "Prise de vue : 30/12/2016."],
+        ["imgs/vanishing5.jpg", "<i>The Vanishing of Ethan Carter</i>, The Astronauts, 2014", "Prise de vue : 30/12/2016."],
+        ["imgs/spaceplan6.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/spaceplan5.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/spaceplan3.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/spaceplan4.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+        ["imgs/spaceplan7.jpg", "<i>Spaceplan</i>, Jake Hollands, Devolver Digital, 2017", "Prise de vue : 16/03/2020."],
+    ],
+//Hyperespace
+    [
+
+        ["imgs/everything1.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 21/03/2018."],
+        ["imgs/everything3.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 21/03/2018."],
+        ["imgs/everything2.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 21/03/2018."],
+        ["imgs/detective1.jpg", "<i>The Darkside Detective</i>, Spooky Doorway, Akapura Games, 2017", "Prise de vue : 02/11/2019."],
+        ["imgs/everything4.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 21/03/2018."],
+        ["imgs/everything5.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 03/04/2018."],
+        ["imgs/everything6.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 03/04/2018."],
+        ["imgs/everything7.jpg", "<i>Everything</i>, David OReilly, 2017", "Prise de vue : 03/04/2018."],
+        ["imgs/letsgopikachu.jpg", "<i>Pokémon: Let's go, Pikachu</i>, Game Freak, Nintendo, 2018", "Prise de vue : 05/05/2022." ],
+        ["imgs/pokemonecarlate1.jpg", "<i>Pokémon Ecarlate</i>, Game Freak, Nintendo, 2022", "Prise de vue: 10/11/2022."],
+        ["imgs/pokemonecarlate2.jpg", "<i>Pokémon Ecarlate</i>, Game Freak, Nintendo, 2022", "Prise de vue: 10/11/2022."],
+        ["imgs/fez.jpg", "<i>FEZ</i>, Polytron Corporation, Trapdoor, 2013", "Prise de vue : 12/03/2018."],
+    ],
+    [
+        ["imgs/observation1.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/observation2.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/observation3.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", "Prise de vue : 28/11/2021."],
+        ["imgs/simulacra2.jpg", "<i>Simulacra 2</i>, Kaigan Games, Neon Doctrine, 2020", "Prise de vue : 29/10/2022"],    
+    ],
+    [
+        ["imgs/observation5.jpg", "<i>Observation</i>, No Code, Devolver Digital, 2020", " Prise de vue : 28/11/2021."],
+    ]
+
+]
